dc280e42c988c3cdbf71e0852c3401d63fcd09e7
0x6d736c <liebem@seas.upenn.edu>
2021-01-04 17:10:10 -0500

    Finalize Virtual Machine with Registers

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * Implementation of a simple virtual machine, with PUSH, ADD, POP, SET, and HALT instructions exposeed.
  **/
@@ -20,19 +18,32 @@ typedef enum {
     HLT
 } ISA;
 
+/**
+ * Defines the enum of the virtual machine's registers.
+ **/
+typedef enum {
+    R0, R1, R2, R3, R4, R5, R6, R7,
+    REGISTER_COUNT
+} Register;
+
 //Creates a test program to process via ISA.
-    const int program[] = {
-        PSH, 30,
-        PSH, 5,
-        ADD,
-        POP,
-        HLT
+const int program[] = {
+    PSH, 30,
+    PSH, 5,
+    ADD,
+    POP,
+    SET, R0, 5,
+    SET, R1, 10,
+    SET, R2, 15,
+    HLT
     };
 
-    int programCounter = 0;         //Program counter. Begins at 0. Used to access instruction to execute.
-    ISA instruction = 0;            //Initialize instruction to 0. Used to store result of fetchInstruction.
-    int stack[256];                 //A stack representation with 256 (0-255) spaces for operations.
-    int stackPointer = -1;          //Pointer for stack operations. 
+Register registers[REGISTER_COUNT]; //Create an array containing the virtual machine's registers.
+
+int programCounter = 0;         //Program counter. Begins at 0. Used to access instruction to execute.
+ISA instruction = 0;            //Initialize instruction to 0. Used to store result of fetchInstruction.
+int stack[256];                 //A stack representation with 256 (0-255) spaces for operations.
+int stackPointer = -1;          //Pointer for stack operations. 
 
 /**
  * Fetches the instruction at programCounter index. 
@@ -44,27 +55,26 @@ int fetchInstruction(const int program[], int * programCounter) {
 void decodeInstruction(ISA instruction) {
     switch (instruction) {
         case PSH:
-            stack[++stackPointer] = program[++programCounter];
+            stack[++stackPointer] = program[++programCounter];                          //Move stackPointer up 1, push the value of programCounter + 1.
             break;
         case ADD:
             stack[stackPointer - 1] = stack[stackPointer] + stack[stackPointer - 1];    //Set stackPointer - 1's stack value to the addition of
                                                                                         //value at stackPointer and value of (stackPointer + 1)
 
             --stackPointer;                                                             //Decrement stackPointer, "popping" the stack.
-            printf("Value after ADD: %d\n", stack[stackPointer]);                   
+            break;                   
         case POP:
             stack[stackPointer] = 0;                                                    //Set stack value at stackPointer index to 0.
             --stackPointer;                                                             //Decrement stackPointer, "popping" the stack.
             break;
         case SET:
-            //TODO: Implement Registers.
+            registers[program[programCounter + 1]] = program[programCounter + 2];       //Set Register to appropriate value.
+            programCounter = programCounter + 2;                                        //Increment programCounter by 2.
             break;
         default:
             break;
     }
 }
-
-
 
 void evaluate(const int program[]) {
     do {
@@ -74,10 +84,10 @@ void evaluate(const int program[]) {
     } while(instruction != HLT);
 }
 
+/**
+ * Begin machine execution. Stop once a HLT is reached. 
+ **/
 int main() {
-    /**
-     * Begin machine execution. Stop once a HLT is reached. 
-     **/
     evaluate(program);
     return 0;
 }
